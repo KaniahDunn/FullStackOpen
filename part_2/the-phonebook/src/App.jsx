00a7772203b6aea7ac14db0e3a7b5580463ff3b2
Kaniah Dunn <kaniahdunn@gmail.com>
2025-02-21 14:40:43 -0500

    updating app to use JSON server and axios

--- part_2/the-phonebook/src/App.jsx
+++ part_2/the-phonebook/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Filter from "./components/Filter";
 import DirectoryList from "./components/DirectoryList";
 import AddPersonForm from "./components/AddPersonForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "617-999-9999", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
